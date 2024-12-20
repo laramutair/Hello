fb66f2e8afaefac923c6793f766aaefe4059d92e
laramutair <lara02mutair@gmail.com>
2024-12-20 09:37:48 +0200

    Assignment 27

--- Assets/Scripts/Assignment27/CallByReference.cs
+++ Assets/Scripts/Assignment27/CallByReference.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+namespace Assignment27
 {
-    // Start is called before the first frame update
+public class CallByReference : MonoBehaviour
+{ 
+    int a;
+    int b;
+    int value;
+    
     void Start()
     {
+        a = 10;
+        b = 0;
+
         
+        Debug.Log("the initial of a is: " + a);
+        CallbyValue(a);
+        Debug.Log("After Call By Value");
+        Debug.Log(a);
+
+        Debug.Log("the initial of b is: " + b);
+        CallbyRef(ref b);
+        Debug.Log("After Call By Ref");
+        Debug.Log(b);
+
+        CallbyOut(out value);
+        Debug.Log("After Call By Out ");
+        Debug.Log(value);
+
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void CallbyValue(int num)
     {
-        
+        num += 10;
     }
+
+    public void CallbyRef(ref int num)
+    {
+        num += 10;
+    }
+
+    public void CallbyOut(out int num)
+    {
+        num = 10;
+    }
+
+    
+}
 }
